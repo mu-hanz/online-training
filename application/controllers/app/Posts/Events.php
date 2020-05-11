1a7fc6408fcd669bc46e02dacb9b6a0f1a395415
MuHanz <muhanz21@gmail.com>
2020-05-12 01:12:16 +0700

    update

--- application/controllers/app/Posts/Events.php
+++ application/controllers/app/Posts/Events.php
@@ -88,6 +88,7 @@ class Events extends CI_Controller
         $this->load->js('assets/app/libs/datatables/dataTables.responsive.min.js');
         $this->load->js('assets/app/libs/datatables/responsive.bootstrap4.min.js');
         $this->load->js('assets/sweetalert/sweetalert2.all.min.js');
+        $this->load->js('assets/app/js/pages/event.datatables.js');
 
         $title = 'List Events';
 
@@ -1364,174 +1365,99 @@ class Events extends CI_Controller
         }
     }
 
-    //json data post
-    public function get_csrf()
+    public function json_event()
     {
-        $this->output->unset_template('layout');
-        $csrf['csrf_name']  = $this->security->get_csrf_token_name();
-        $csrf['csrf_token'] = $this->security->get_csrf_hash();
-        echo json_encode($csrf);
-    }
+        
+        $this->output->unset_template();
 
-    public function json_post($type, $filter, $author = false)
-    {
-        $this->output->unset_template('layout');
-
-        function is_tags($id_post, $type)
+        function is_img($event_thumbs)
         {
-            $ci = &get_instance();
-
-            $data_tags = $ci->Terms_m->get_terms_json('terms.name,terms.slug', 'tags', $id_post)->result();
-            $data      = array();
-            foreach ($data_tags as $key) {
-                $data[] = '<a href="' . base_url('webadmin/post?type=' . $type . '&filter=' . $key->slug) . '" class="ajax-list">' . $key->name . '</a>';
-            }
-            $comma_separated = implode(", ", $data);
-            if ($comma_separated == '') {
-                $tags = '—';
+            if(empty($event_thumbs)){
+                $event_thumbs = 'assets/app/images/img-default.png';
             } else {
-                $tags = $comma_separated;
-            }
-            return $tags;
-
-        }
-
-        function is_cat($id_post, $post_type)
-        {
-            $ci = &get_instance();
-
-            if ($post_type == 'events') {
-                $type_cat = 'category-events';
-            } else {
-                $type_cat = 'category';
+                $event_thumbs = 'media/'.$event_thumbs;
             }
 
-            $data_tags = $ci->Terms_m->get_terms_json('terms.name,terms.slug', $type_cat, $id_post)->result();
-            $data      = array();
-            foreach ($data_tags as $key) {
-                $data[] = '<a href="' . base_url('webadmin/post?type=' . $post_type . '&filter=' . $key->slug) . '" class="ajax-list">' . $key->name . '</a>';
-            }
-            $comma_separated = implode(", ", $data);
-
-            if ($comma_separated == '') {
-                $cat = '—';
-            } else {
-                $cat = $comma_separated;
-            }
-            return $cat;
-
-        }
-
-        function is_title($id_post, $post_title, $post_slug, $post_status, $post_type, $post_trash)
-        {
-
-            if ($post_trash == '1') {
-                $data = '
-                      <div class="row-action" id="action' . $id_post . '">
-                        <span><a class="ajax-link" href="' . base_url('webadmin/post/restore/' . $post_type . '/' . $id_post) . '">Pulihkan</a></span>
-                        <span><a class="ajax-link del" href="' . base_url('webadmin/post/delete_permanent/' . $post_type . '/' . $id_post) . '">Hapus Selamanya</a></span>
-                      </div>
-                      ';
-            } else {
-
-                if ($post_status == 'draft') {
-                    if ($post_type == 'events') {
-                        $type_post_link = 'event';
-                    } elseif ($post_type == 'pages') {
-                        $type_post_link = 'pages';
-                    } else {
-                        $type_post_link = 'blog';
-                    }
-
-                    $link_view = base_url($type_post_link . '/preview/' . $post_slug);
-                    $text      = 'Preview';
-
-                } else {
-                    if ($post_type == 'events') {
-                        $type_post_link = 'event';
-                    } elseif ($post_type == 'pages') {
-                        $type_post_link = 'pages';
-                    } else {
-                        $type_post_link = 'blog';
-                    }
-                    $link_view = base_url($type_post_link . '/' . $post_slug);
-                    $text      = 'View';
-                }
-
-                $data = '
-                      <div class="row-action" id="action' . $id_post . '">
-                        <span><a class="ajax-list" href="' . base_url('webadmin/post/edit_post/' . $post_type . '/' . $id_post) . '">Edit</a></span>
-                        <span><a class="ajax-link del" href="' . base_url('webadmin/post/move_trash/' . $post_type . '/' . $id_post) . '">Delete</a></span>
-                        <span><a href="' . $link_view . '" target="blank">' . $text . '</a></span>
-                      </div>
-                      ';
-            }
-
+            $data = ' <img class="card-img-top img-fluid" src="'.base_url($event_thumbs).'" id="thumbs" style="width:170px">';
+            
             return $data;
         }
 
-        function is_status($post_status)
+
+        function is_date($event_start_date, $event_start_time, $event_end_date, $event_end_time, $event_duration)
         {
-            if ($post_status == 'draft') {
-                $status = '— <span class="text-draft">' . ucwords($post_status) . '</span>';
-                return $status;
-            } elseif($post_status == 'on_schedule'){
-                $status = '— <span class="text-scheduled">Scheduled</span>';
-                return $status;
-            }
+            $start_date = date("d M Y H:i:s", strtotime($event_start_date.' '.$event_start_time));
+            $end_date = date("d M Y H:i:s", strtotime($event_start_date.' '.$event_start_time));
+
+            $data = 'Start Date : '.$start_date.'<br> End Date : '.$end_date.'<br> Duration : '.$event_duration;
+            
+            return $data;
         }
 
-        function is_date($post_status, $post_modifed, $post_date)
+        function is_event($event_name, $event_subtitle, $event_sku, $category, $type, $group, $certificate, $regional, $address, $location)
         {
-            $date_draft = date("d M Y", strtotime($post_modifed));
-            $date_pubsh = date("d M Y", strtotime($post_date));
-
-            if ($post_status == 'draft') {
-                $date = '<span class="text-muted">Terakhir diedit<br><abbr title="' . $post_modifed . '">' . $date_draft . '</abbr></span>';
-            } elseif($post_status == 'on_schedule') {
-                $date = '<span style="color:#63c381;">Scheduled<br><abbr title="' . $post_date . '">' . $date_pubsh . '</abbr></span>';
-            } else {
-                $date = '<span class="text-muted">Published<br><abbr title="' . $post_date . '">' . $date_pubsh . '</abbr></span>';
+            
+            $type_to_initial = explode(" ", $type);
+            $type_initial = "";
+            foreach ($type_to_initial as $i) {
+                $type_initial .= $i[0];
             }
 
-            return $date;
+            $data = '
+            <div class="row">
+                <div class="col-lg-8 mb-2">
+                    <strong>'.$event_name.' <i>('.$type_initial.')</i></strong><br>
+                    <small><i>'.$event_subtitle.'</i></small>
+                </div>
+                <div class="col-lg-6">
+                    Type : '.$type.'<br>
+                    SKU : '.$event_sku.'<br>
+                    Category : '.$category.'
+                </div>
+                <div class="col-lg-6">
+                    Group : '.$group.'<br>
+                    Certificate : '.$certificate.'<br>
+                    Regional : '.$regional.'
+                </div>
+                <div class="col-lg-12">
+                    Location : '.$location.' ('.$address.')
+                </div>
+            </div>
+        ';
+            
+            return $data;
         }
 
-        $this->datatables->select('id_post, post_title, post_type, post_trash, first_name,post_author, post_status, post_date, post_slug, post_modifed');
-        $this->datatables->from('posts');
-        $this->datatables->join('users', 'users.id = posts.post_author', 'left');
-        $this->datatables->join('term_relationships', 'term_relationships.object_id = posts.id_post', 'left');
-        $this->datatables->join('terms', 'terms.term_id = term_relationships.term_taxonomy_id', 'left');
-        $this->datatables->where('post_type', $type);
-
-        if ($filter == 'all') {
-            $this->datatables->where('post_trash != "1"');
-        } elseif ($filter == 'trash') {
-            $this->datatables->where('post_trash', '1');
-        } elseif ($filter == 'on_schedule') {
-            $this->datatables->where('post_trash', '0');
-            $this->datatables->where('post_status', $filter);
-        } elseif ($filter == 'draft' || $filter == 'publish') {
-            $this->datatables->where('post_trash', '0');
-            $this->datatables->where('post_status', $filter);
-        } else {
-            $this->datatables->where('post_trash', '0');
-            $this->datatables->where('terms.slug', $filter);
+        function is_action($event_id)
+        {
+    
+            $data = '<button class="btn btn-primary btn-sm btn-block"><i class="uil uil-edit"></i> Edit</button>
+            <br>
+            <button class="btn btn-danger btn-sm  btn-block"><i class="uil uil-trash-alt"></i> Delete</button>';
+            
+            return $data;
         }
+      
 
-        if ($author) {
-            $this->datatables->where('post_author', $author);
-        }
+        $this->datatables->select('event_id, event_thumbs, event_name, event_subtitle, event_sku, event_duration, event_start_date, event_start_time, event_end_date, event_end_time, c.name as category, t.name as type, g.name as group, ct.name as certificate, r.name as regional, l1.description as address, l.name as location');
+        $this->datatables->from('events');
+        $this->datatables->join('terms as c', 'c.term_id = events.category_id', 'left');
+        $this->datatables->join('terms as t', 't.term_id = events.type_id', 'left');
+        $this->datatables->join('terms as g', 'g.term_id = events.group_id', 'left');
+        $this->datatables->join('terms as ct', 'ct.term_id = events.certificate_id', 'left');
+        $this->datatables->join('terms as r', 'r.term_id = events.regional_id', 'left');
+        $this->datatables->join('terms as l', 'l.term_id = events.location_id', 'left');
+        $this->datatables->join('term_taxonomy as l1', 'l1.term_id = l.term_id', 'left');
+        $this->datatables->where('event_status != "deleted"');
+        $this->datatables->order_by('event_id', 'desc');
+        $this->datatables->group_by('event_id');
 
-        $this->datatables->order_by('id_post', 'desc');
-        $this->datatables->group_by('id_post');
-        $this->datatables->add_column('title', '$1', "is_title('id_post','post_title','post_slug','post_status','post_type','post_trash')");
-        $this->datatables->add_column('category', '$1', "is_cat('id_post','post_type')");
-        $this->datatables->add_column('tags', '$1', "is_tags('id_post','post_type')");
-        $this->datatables->add_column('author', '<a href="' . base_url() . 'webadmin/post?type=' . $type . '&filter=all&author=$2">$1</a>', 'first_name,post_author');
-        $this->datatables->add_column('date', '$1', "is_date('post_status','post_modifed','post_date')");
-        $this->datatables->add_column('status', '$1', "is_status('post_status')");
-
+        $this->datatables->add_column('detail_event', '$1', "is_event('event_name','event_subtitle','event_sku','category','type','group','certificate','regional','address', 'location')");
+        $this->datatables->add_column('event_date', '$1', "is_date('event_start_date','event_start_time','event_end_date','event_end_time','event_duration')");
+        $this->datatables->add_column('event_img', '$1', "is_img('event_thumbs')");
+        $this->datatables->add_column('action', '$1', "is_action('event_id')");
+        
+      
         return print_r($this->datatables->generate());
     }
 
--- application/views/app/post/event_list.php
+++ application/views/app/post/event_list.php
@@ -13,51 +13,17 @@
         <div class="col-xl-12">
 
                 <div class="table-responsive show-table">
-                    <table id="datatable" class="table table-hover data-list-view">
+                    <table id="datatable-event" class="table table-hover data-list-view">
                         <thead>
                         <tr>
-                            <th width="200px">Name</th>
-                            <th>Description</th>
-                            <th>Slug</th>
-                            <th width="35px">Count</th>
+                            <th width="200px">Images</th>
+                            <th>Event Detail</th>
+                            <th>Date</th>
+                            <th>Action</th>
                         </tr>
                         </thead>
-                        <tbody class="body-row">
-                            <tr>
-                                <td class="p-0">
-                                    <img class="card-img-top img-fluid" src="<?php echo base_url('assets/app/images/small/img-1.jpg');?>" id="thumbs" width="50%">
-                                </td>
-                                <td class="pt-1 pb-1">
-                                    <div class="row">
-                                        <div class="col-lg-8 mb-2">
-                                            <strong>Nama event <i>(VT)</i></strong><br>
-                                            SKU : <i>Subtutle</i>
-                                        </div>
-                                        <div class="col-lg-6">
-                                        Type : dasdasd<br>
-                                        Group : dasdasd
-                                        </div>
-                                        <div class="col-lg-6">
-                                        Certificate : dasdasd<br>
-                                        Regional : dasdasd
-                                        </div>
-                                        <div class="col-lg-6">
-                                            Category : dasdasd
-                                        </div>
-                                            <div class="col-lg-6">
-                                            Location : dasdasd
-                                        </div>
-                                    </div>
-                                    
-                                   
-
-                                </td>
-                                <td  class="pt-1 pb-1">Start Date :<br> dasdasd<br>
-                                        End Date :<br> dasdasd<br>
-                                        Duration :<br> sdasdasd
-                                    </td>
-                                <td>adda</td>
-                            </tr>
+                        <tbody>
+                         
                         </tbody>
                     </table>
                     </div>
