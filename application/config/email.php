0346b5de2fa37d5f900a2c0f6a4e8e198d0924cf
MuHanz <44943686+mu-hanz@users.noreply.github.com>
2020-11-05 19:02:48 +0700

    Update email.php

--- application/config/email.php
+++ application/config/email.php
@@ -3,8 +3,8 @@
 $config['protocol']     = 'smtp';
 $config['smtp_host']    = 'smtp.gmail.com';
 $config['smtp_port']    = '465';
-$config['smtp_user']    = 'hanzzame77@gmail.com';
-$config['smtp_pass']    = 'hanzzame312u7';
+$config['smtp_user']    = 'email@gmail.com';
+$config['smtp_pass']    = 'password';
 $config['smtp_crypto']     = 'ssl';
 $config['mailtype']     = 'html';
 $config['charset']      = 'iso-8859-1';
